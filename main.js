6ba352da7de631a302a856c9a3f4eea01622383c
TheRealBasel <alanaziibasel@gmail.com>
2021-10-10 15:37:42 +0300

    Finsih HW

--- main.js
+++ main.js
@@ -28,3 +28,83 @@ const characters = [
         gender: 'male',
     },
 ];
+
+/* Get array of all names */
+let namesArray = characters.map((character) => {
+    return character.name
+})
+
+console.log ( namesArray );
+
+/* Get array of all heights */
+
+let heightsArray = characters.map((character) => {
+    return character.height;
+})
+
+console.log ( heightsArray );
+
+/* Get total height of all characters */
+
+let heightTotal = heightsArray.reduce((total, num) => {
+    return total + num;
+})
+
+console.log ( heightTotal );
+
+/* Get characters with height less than 200 */
+
+let lessHeight = characters.filter((value, key) => {
+    return value.height < 200;
+})
+
+console.log ( lessHeight );
+
+/* Get all male characters */
+
+let maleCharacters = characters.filter((value, key) => {
+    return value.gender == "male";
+})
+
+console.log ( maleCharacters );
+
+/* Sort by mass */
+let massSortedharacters = characters.sort((a, b) => {
+    return a.mass < b.mass;
+})
+
+console.log ( massSortedharacters );
+/* Sort by height */
+let heightSortedharacters = characters.sort((a, b) => {
+    return a.height < b.height;
+})
+
+console.log ( heightSortedharacters );
+
+/* Does every character have mass more than 40? */
+let massMoreThanCharacters = characters.every((value) => {
+    return value.mass > 40;
+})
+
+console.log ( massMoreThanCharacters );
+
+/* Does every character have mass more than 40? */
+let shorterThanCharacters = characters.every((value) => {
+    return value.height < 200;
+})
+
+console.log ( shorterThanCharacters );
+
+/* Is there at least one character with blue eyes? */
+let isThereBlueeyesCharacters = characters.some((value) => {
+    return value.eye_color === "blue";
+})
+
+console.log ( isThereBlueeyesCharacters );
+
+/* Is there at least one character with blue eyes? */
+let isThereTallerCharacter = characters.some((value) => {
+    return value.height > 210;
+})
+
+console.log ( isThereTallerCharacter );
